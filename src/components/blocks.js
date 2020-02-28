78efb7bf81af5d104b05d8d34310c3502194352c
Lachlan Campbell <lachlan@hackclub.com>
2020-02-28 05:07:08 +0000

    Fix YouTube import

--- src/components/blocks.js
+++ src/components/blocks.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
 import React from 'react'
-import { YouTubePlayer } from 'react-player'
+import YouTubePlayer from 'react-player/lib/players/YouTube'
 
 export const Container = ({ wide, ...props }) => (
   <div
