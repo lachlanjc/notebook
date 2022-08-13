a2c9c0ffbd04bdced23441e9ba2c44c403408a44
Lachlan Campbell <lachlanjc@hey.com>
2022-08-13 01:38:22 -0400

    Drop extension from card URLs

--- components/blocks.jsx
+++ components/blocks.jsx
@@ -73,6 +73,7 @@ export const CodeSandbox = ({ id, file, ...props }) => {
         overflow: 'hidden',
       }}
       title={`${initial(id.split('-'))} on CodeSandbox`}
+      loading="lazy"
       allow="accelerometer; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; xr-spatial-tracking"
       sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
       {...props}
