f07ac7eae19249abd0c47ec7c011c817828f0ec4
Lachlan Campbell <lachlanjc@hey.com>
2025-02-21 13:20:51 -0500

    Add vc analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,14 @@ import * as React from 'react'
 import theme from '../lib/theme'
 import components from '../lib/components'
 import { ThemeProvider } from 'theme-ui'
+import { Analytics } from '@vercel/analytics/next'
 import '../lib/fonts.css'
 
 const App = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme} components={components}>
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   )
 }
